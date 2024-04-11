c478187364135dde1b6b820e32028f5aae5b1782
Livsy90 <artmir@live.ru>
2024-04-11 09:47:17 +0400

    Initial Commit

---
+++ ProjectSUIRedux/ProjectSUIReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSUIReduxApp.swift
+//  ProjectSUIRedux
+//
+//  Created by Livsy on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSUIReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
